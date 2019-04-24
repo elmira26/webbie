3fe1b45ccf2ca8f8cdfdcc1c5a5fc60245f9b9ae
lle <e-ve-j@hotmail.com>
2019-04-24 10:20:43 +0300

    hj

--- file.js
+++ file.js
@@ -1,3 +1,8 @@
 function changefon() {
-document.getElementById("changefon").style.background = "#fefaf2"; 
+
+  clr = 'background: -moz-linear-gradient(top, #F5EBEB 150%, #F0F5EB 150%, #EBF5F5 150%);'
+  clr += 'background: -webkit-linear-gradient(top, #F5EBEB 150%, #F0F5EB 150%, #EBF5F5 0%);'
+  clr += 'background: linear-gradient(to bottom, #F5EBEB 0%, #F0F5EB 0%, #EBF5F5 150%);'
+    document.getElementById("changefon").style.cssText  = clr;
+       
 }
